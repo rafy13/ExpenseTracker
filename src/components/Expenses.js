bd3269403797ee78ec457110f69699964b5d8dc7
A. K. M. Rabby <rafy020395@gmail.com>
2021-11-27 00:11:48 +0600

    Implement expenses component

---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+	return (
+		<div className="expenses">
+			{props.items.map((item) => {
+				return (
+					<ExpenseItem
+						title={item.title}
+						amount={item.amount}
+						date={item.date}
+					></ExpenseItem>
+				);
+			})}
+		</div>
+	);
+}
+
+export default Expenses;
