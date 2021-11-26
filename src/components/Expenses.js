b39d36988e9613dda3f5541a9399536b92dc01d0
A. K. M. Rabby <rafy020395@gmail.com>
2021-11-27 00:24:58 +0600

    Implement wrapper component card, that takes the class names as props and use props.children inside.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
 	return (
-		<div className="expenses">
+		<Card className="expenses">
 			{props.items.map((item) => {
 				return (
 					<ExpenseItem
@@ -13,7 +14,7 @@ function Expenses(props) {
 					></ExpenseItem>
 				);
 			})}
-		</div>
+		</Card>
 	);
 }
 
