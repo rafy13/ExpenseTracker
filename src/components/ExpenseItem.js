a45873c1891eb8f4f7a4034eb03372df2287e6a0
A. K. M. Rabby <rafy020395@gmail.com>
2021-11-26 10:33:51 +0600

    Implement Expense Item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (<div>
+      <div>March 2nd 1995</div>
+      <div>
+          <h2>Insurance</h2>
+          <div>1000.00</div>
+      </div>
+  </div>);
+}
+
+export default ExpenseItem;
