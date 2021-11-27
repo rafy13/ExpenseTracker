d0db8180f4fcd54f1158e57658c45bfeec7ab5b1
A. K. M. Rabby <rafy020395@gmail.com>
2021-11-27 12:41:09 +0600

    Add year filter select element and it's conrols

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,35 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+	const [filteredYear, setFilteredYear] = useState("2020");
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
 	return (
-		<Card className="expenses">
-			{props.items.map((item) => {
-				return (
-					<ExpenseItem
-						title={item.title}
-						amount={item.amount}
-						date={item.date}
-					></ExpenseItem>
-				);
-			})}
-		</Card>
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
+				/>
+				{props.items.map((item) => {
+					return (
+						<ExpenseItem
+							title={item.title}
+							amount={item.amount}
+							date={item.date}
+						></ExpenseItem>
+					);
+				})}
+			</Card>
+		</div>
 	);
 };
 
